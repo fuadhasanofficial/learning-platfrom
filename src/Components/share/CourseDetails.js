5973d99c9224f87a8ab2c51ca92da5f1125957ad
fuadhasanofficial <freefireind9807@gmail.com>
2023-08-23 11:56:27 +0600

    course details see more and see less toggle , or footer section is done

--- src/Components/share/CourseDetails.js
+++ src/Components/share/CourseDetails.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
+import { Icon } from '@iconify/react';
 
 const CourseDetails = () => {
 
     const details = useLoaderData();
+    const fullDetails = details.details;
+    const shortDetails = fullDetails.slice(0, 500)
+    const [courseDetails, setcourseDetails] = useState(shortDetails);
+    const [seeMorebutton, setButton] = useState(false)
+    const handleSeeMore = () => {
+        setButton(!seeMorebutton);
+        if (seeMorebutton) {
+            setcourseDetails(fullDetails)
+        } else (setcourseDetails(shortDetails))
+    }
     return (
-        <div className='container'>
-            <div>
-                <Card>
-                    <Card.Img variant="top" src={details.image} />
-                    <Card.Body>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
-        </div>
+        <Row>
+            <Col lg={6} className='mx-auto'>
+                <div className='container w-lg-50 mt-10'>
+                    <div>
+                        <Card>
+                            <Card.Img variant="top" src={details.image} className='img-fluid ' />
+                            <Card.Body>
+                                <Card.Text>
+                                    <h4 className='text-center'>{details.name}</h4>
+
+                                    <div className='d-flex justify-content-between'>
+                                        <div>
+                                            <Icon icon="ri:time-line" /> {details.duration} Month
+                                        </div>
+                                        <div>
+                                            <Icon icon="mdi:currency-bdt" color="#a24dd6" />{details.course_fee} BDT
+                                        </div>
+                                        <div>
+                                            <Icon icon="healthicons:i-training-class" color="#24d47c" />{details.total_class} classes
+                                        </div>
+                                        <div>
+                                            <Icon icon="mingcute:code-fill" color="#e04878" /> {details.language}
+                                        </div>
+                                    </div>
+                                </Card.Text>
+
+                                <div>
+                                    <p>{courseDetails} <button onClick={handleSeeMore}>{seeMorebutton ? 'see more' : 'see less'}</button> </p>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+            </Col>
+        </Row>
+
     );
 };
 
---
+++ src/Components/share/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='"position-absolute top-100 text-center footer'>
+            <h4>Your skill </h4>
+            <p>Founder : Fuad Hasan </p>
+        </div>
+    );
+};
+
+export default Footer;
