5cf6eb47996d62631f2236fc77a4f95619a2dc55
fuadhasanofficial <freefireind9807@gmail.com>
2023-08-29 19:41:25 +0600

    auth iformation hide Done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAKRZ0yEgacBJp-JxdAgaqtlFqp8X7Hj-I",
-    authDomain: "your-skill.firebaseapp.com",
-    projectId: "your-skill",
-    storageBucket: "your-skill.appspot.com",
-    messagingSenderId: "443292367870",
-    appId: "1:443292367870:web:fbde4e89b154d00239deaf",
-    measurementId: "G-61LHCKPPYP"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
